c6e5902d76ee4d215c46f48ad2b0ef9066a693cf
Leonard Missbach <missbach@mcc-berlin.net>
2022-11-24 09:24:08 +0100

    Updated cleaning Ethiopia

--- 0_Cleaning_Ethiopia.R
+++ 0_Cleaning_Ethiopia.R
@@ -1,75 +1,90 @@
+if(!require("pacman")) install.packages("pacman")
+
+p_load("countrycode", "haven", "Hmisc", "openxlsx", "rattle", "scales", "tidyverse", "sjlabelled")
+
+
 # Load Data ####
-cover <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect_cover_hh_w4.dta")
-sect1_hh_w4 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect1_hh_w4.dta")
-# sect1_ph_w4 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect1_ph_w4.dta")
-# sect1_pp_w4 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect1_pp_w4.dta")
-
-sect2 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect2_hh_w4.dta")
-sect4 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect4_hh_w4.dta")
-
-sect6a <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect6a_hh_w4.dta")
-# sect6b1 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect6b1_hh_w4.dta")
-# sect6b2 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect6b2_hh_w4.dta")
-# sect6b3 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect6b3_hh_w4.dta")
-sect6b4 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect6b4_hh_w4.dta")
-
-sect7a1 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect7a_hh_w4.dta")
-sect7a2 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect7b_hh_w4_v2.dta")
-sect10a <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect10a_hh_w4.dta")
-
-
-sect11 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect11_hh_w4.dta")
-#sec11b1 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect11b1_hh_w4.dta")
-#sec11b2 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect11b2_hh_w4.dta")
-
-#com_03 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect03_com_w4.dta")
-#com_04 <- read_dta("C:/Users/misl/ownCloud/Fuel_Elasticities_in_LMIC/1_Ethiopia/Data/ETH_2018_ESS_v02_M_Stata/sect04_com_w4.dta")
+cover       <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect_cover_hh_w4.dta")
+sect1       <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect1_hh_w4.dta")
+sect2       <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect2_hh_w4.dta")
+sect4       <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect4_hh_w4.dta")
+sect6a      <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect6a_hh_w4.dta")
+sect6b1     <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect6b1_hh_w4.dta")
+sect6b2     <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect6b2_hh_w4.dta")
+sect6b3     <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect6b3_hh_w4.dta")
+sect6b4     <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect6b4_hh_w4.dta")
+sect7a1     <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect7a_hh_w4.dta")
+sect7a2     <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect7b_hh_w4_v2.dta")
+sect10a     <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect10a_hh_w4.dta")
+sect11      <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect11_hh_w4.dta")
+sec11b1     <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect11b1_hh_w4.dta")
+sec11b2     <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect11b2_hh_w4.dta")
+sec13       <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect13_hh_w4_v2.dta")
+sec14       <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect14_hh_w4.dta")
+com_03      <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect03_com_w4.dta")
+com_04      <- read_dta("../0_Data/1_Household Data/2_Ethiopia/1_Data_Raw/ETH_2018_ESS_v02_M_Stata/sect04_com_w4.dta")
 
 
 # Note: Spacial Mapping is possible
 cover_0 <- cover %>%
-  select(household_id, ea_id, saq14, pw_w4, saq01, saq09)%>%
+  select(household_id, ea_id, saq14, pw_w4, saq01, saq09, saq03)%>%
   mutate(urban_01 = ifelse(saq14 == 1,0,1))%>%
-  rename(hh_weights = pw_w4, hh_size = saq09, district = saq01)%>%
+  rename(hh_id = household_id, hh_weights = pw_w4, hh_size = saq09, province = saq01, district = saq03)%>% # district = woreda
   select(-saq14)
 
-sect_1.1 <- sect1_hh_w4 %>%
+sect_1.1 <- sect1 %>%
   select(household_id, individual_id, s1q03a, s1q04)%>%
-  rename(age = s1q03a)%>%
-  mutate(adults = ifelse(age > 17,1,0),
-         children = ifelse(age <18,1,0))%>%
-  group_by(household_id)%>%
-  summarise(adults = sum(adults),
+  rename(age      = s1q03a, hh_id = household_id)%>%
+  mutate(adults   = ifelse(age > 15,1,0),
+         children = ifelse(age < 16,1,0))%>%
+  group_by(hh_id)%>%
+  summarise(adults    = sum(adults),
              children = sum(children))%>%
   ungroup()
 
-sect_1.2 <- sect1_hh_w4 %>%
+sect_1.2 <- sect1 %>%
   filter(s1q01 == 1)%>%
   select(household_id, individual_id, s1q08, s1q02)%>%
-  rename(religion = s1q08, sex_hhh = s1q02)
+  rename(hh_id = household_id, religion = s1q08, sex_hhh = s1q02)
 
 sect_2.1 <- sect2 %>%
   select(household_id, individual_id, s2q06)%>%
-  rename(edu_hhh = s2q06)
+  rename(edu_hhh = s2q06, hh_id = household_id)
 
-sect_1.2.1 <- left_join(sect_1.2, sect_2.1)
+sect4.1 <- sect4 %>%
+  rename(hh_id = household_id, ind_hhh = s4q34d)%>%
+  select(hh_id, ind_hhh, individual_id)
 
-cover_1 <- left_join(cover_0, sect_1.1)%>%
-  left_join(sect_1.2.1)%>%
-  remove_all_labels()%>%
+sect_1.2.1 <- left_join(sect_1.2, sect_2.1)%>%
+  left_join(sect4.1)%>%
   select(-individual_id)%>%
-  rename(hh_id = household_id)
-
-housing <- sect10a %>%
-  select(household_id, s10aq12, s10aq21, s10aq34, s10aq38, s10aq41)%>%
-  rename(telephone = s10aq41, cooking_fuel = s10aq38, lighting_fuel = s10aq34, toilet = s10aq12, water = s10aq21, hh_id = household_id)
-
-cover_2 <- left_join(cover_1, housing)%>%
   remove_all_labels()
 
-write_csv(cover_2, "../0_Data/1_Household Data/2_Ethiopia/1_Data_Clean/household_information_Ethiopia.csv")
+housing <- sect10a %>%
+  select(household_id, s10aq12, s10aq21, s10aq34, s10aq38)%>%
+  rename(cooking_fuel = s10aq38, lighting_fuel = s10aq34, toilet = s10aq12, water = s10aq21, hh_id = household_id)%>%
+  mutate(electricity.access = ifelse(lighting_fuel %in% c(1,2,3,4),1,0))
 
+# sec13.1 <- sec13 %>%
+#   rename(hh_id = household_id)%>%
+#   filter(source_cd == 105)
 
+sec14.1 <- sec14 %>%
+  rename(hh_id = household_id)%>%
+  filter(assistance_cd != 2)%>%
+  mutate(inc_gov_cash = ifelse(!is.na(s14q03), s14q03,0),
+         inc_gov_monetary = 0)%>%
+  group_by(hh_id)%>%
+  summarise(inc_gov_cash = sum(inc_gov_cash),
+            inc_gov_monetary = sum(inc_gov_monetary))%>%
+  ungroup()
+
+household_information <- cover_0 %>%
+  left_join(sect_1.2.1)%>%
+  left_join(housing)%>%
+  left_join(sec14.1)
+
+write_csv(household_information, "../0_Data/1_Household Data/2_Ethiopia/1_Data_Clean/household_information_Ethiopia.csv")
 
 # Expenditures
 
@@ -129,8 +144,6 @@ write_csv(consumption, "../0_Data/1_Household Data/2_Ethiopia/1_Data_Clean/expen
 
 # Appliances
 
-
-
 appliances <- sect11 %>%
   rename(hh_id = household_id)%>%
   select(hh_id, asset_cd, s11q00)%>%
@@ -140,8 +153,8 @@ appliances <- sect11 %>%
                                           ifelse(asset_cd == 8, "radio.01",
                                                  ifelse(asset_cd == 9, "tv.01",
                                                         ifelse(asset_cd == 14, "motorcycle.01",
-                                                               ifelse(asset_cd == 21, "refrigerator",
-                                                                      ifelse(asset_cd == 22, "car",
+                                                               ifelse(asset_cd == 21, "refrigerator.01",
+                                                                      ifelse(asset_cd == 22, "car.01",
                                                                              ifelse(asset_cd == 23, "other", "other"))))))))))%>%
   filter(Appliance != "other")%>%
   mutate(yn = ifelse(s11q00 == 1,1,0))%>%
@@ -150,39 +163,44 @@ appliances <- sect11 %>%
 
 write_csv(appliances, "../0_Data/1_Household Data/2_Ethiopia/1_Data_Clean/appliances_0_1_Ethiopia.csv")
 
-
-
 # Codierungen
 
-district.code <- stack(attr(cover_0$district, 'labels'))%>%
-  rename(district = values, District = ind)
-
-religion.code <- stack(attr(sect_1.2.1$religion, 'labels'))%>%
-  rename(religion = values, Religion = ind)
-
-gender.code <- stack(attr(sect_1.2.1$sex_hhh, 'labels'))%>%
-  rename(sex_hhh = values, SEX_HHH = ind)
-
-education.code <- stack(attr(sect_1.2.1$edu_hhh, 'labels'))%>%
-  rename(edu_hhh = values, EDU_HHH = ind)
-
-lighting.code <- stack(attr(housing$lighting_fuel, 'labels'))%>%
-  rename(lighting_fuel = values, Lighting = ind)
-
-cooking.code <- stack(attr(housing$cooking_fuel, 'labels'))%>%
-  rename(cooking_fuel = values, Cooking = ind)
-
-water.code <- stack(attr(housing$water, 'labels'))%>%
-  rename(water = values, Water = ind)
-
-toilet.code <- stack(attr(housing$toilet, 'labels'))%>%
-  rename(toilet = values, Toilet = ind)
+Province.Code <- stack(attr(cover$saq01, 'labels'))%>%
+  rename(province = values, Province = ind)%>%
+  write_csv(., "../0_Data/1_Household Data/2_Ethiopia/2_Codes/Province.Code.csv")
+District.Code <- distinct(cover, saq03)%>%
+  arrange(saq03)%>%
+  rename(district = saq03)%>%
+  write_csv(., "../0_Data/1_Household Data/2_Ethiopia/2_Codes/District.Code.csv")
+Religion.Code <- stack(attr(sect1$s1q08, 'labels'))%>%
+  rename(religion = values, Religion = ind)%>%
+  write_csv(., "../0_Data/1_Household Data/2_Ethiopia/2_Codes/Religion.Code.csv")
+Gender.Code <- stack(attr(sect1$s1q02, 'labels'))%>%
+  rename(sex_hhh = values, Gender = ind)%>%
+  write_csv(., "../0_Data/1_Household Data/2_Ethiopia/2_Codes/Gender.Code.csv")
+Education.code <- stack(attr(sect2$s2q06, 'labels'))%>%
+  rename(edu_hhh = values, Education = ind)%>%
+  write_csv(., "../0_Data/1_Household Data/2_Ethiopia/2_Codes/Education.Code.csv")
+Lighting.code <- stack(attr(sect10a$s10aq34, 'labels'))%>%
+  rename(lighting_fuel = values, Lighting_Fuel = ind)%>%
+  write_csv(., "../0_Data/1_Household Data/2_Ethiopia/2_Codes/Lighting.Code.csv")
+Cooking.code <- stack(attr(sect10a$s10aq38, 'labels'))%>%
+  rename(cooking_fuel = values, Cooking_Fuel = ind)%>%
+  write_csv(., "../0_Data/1_Household Data/2_Ethiopia/2_Codes/Cooking.Code.csv")
+Water.code <- stack(attr(sect10a$s10aq21, 'labels'))%>%
+  rename(water = values, Water = ind)%>%
+  write_csv(., "../0_Data/1_Household Data/2_Ethiopia/2_Codes/Water.Code.csv")
+Toilet.code <- stack(attr(sect10a$s10aq12, 'labels'))%>%
+  rename(toilet = values, Toilet = ind)%>%
+  write_csv(., "../0_Data/1_Household Data/2_Ethiopia/2_Codes/Toilet.Code.csv")
+Industry.Code <- stack(attr(sect4$s4q34d, 'labels'))%>%
+  rename(ind_hhh = values, Industry = ind)%>%
+  write_csv(., "../0_Data/1_Household Data/2_Ethiopia/2_Codes/Industry.Code.csv")
 
 Item.1 <- stack(attr(sect_6a$item_code, 'labels'))%>%
   rename(item_code = values, item_name = ind)
 Item.2 <- stack(attr(sect_6b4$item_code, 'labels'))%>%
   rename(item_code = values, item_name = ind)
-
 Item.3 <- stack(attr(sect7a1$item_cd_30day, 'labels'))%>%
   rename(item_code = values, item_name = ind)%>%
   mutate(item_code = item_code + 1000)
@@ -193,20 +211,3 @@ Item.4 <- stack(attr(sect7a2$item_cd_12months, 'labels'))%>%
 Items <- bind_rows(Item.1, Item.2, Item.3, Item.4)
 
 write.xlsx(Items, "../0_Data/1_Household Data/2_Ethiopia/3_Matching_Tables/Item_Codes_Description_Ethiopia.xlsx")
-
-write_csv(toilet.code ,"../0_Data/1_Household Data/2_Ethiopia/2_Codes/toilet.code.csv")
-write_csv(water.code ,"../0_Data/1_Household Data/2_Ethiopia/2_Codes/water.code.csv")
-write_csv(lighting.code ,"../0_Data/1_Household Data/2_Ethiopia/2_Codes/lighting.code.csv")
-write_csv(cooking.code ,"../0_Data/1_Household Data/2_Ethiopia/2_Codes/cooking.code.csv")
-write_csv(gender.code ,"../0_Data/1_Household Data/2_Ethiopia/2_Codes/gender.code.csv")
-write_csv(district.code ,"../0_Data/1_Household Data/2_Ethiopia/2_Codes/district.code.csv")
-write_csv(education.code ,"../0_Data/1_Household Data/2_Ethiopia/2_Codes/education.code.csv")
-write_csv(religion.code ,"../0_Data/1_Household Data/2_Ethiopia/2_Codes/religion.code.csv")
-
-household    <- read_csv("../0_Data/1_Household Data/2_Ethiopia/1_Data_Clean/household_information_Ethiopia.csv")
-expenditures <- read_csv("../0_Data/1_Household Data/2_Ethiopia/1_Data_Clean/expenditures_items_Ethiopia.csv")
-
-household <- household %>%
-  filter(hh_id %in% expenditures$hh_id)
-
-write_csv(household, "../0_Data/1_Household Data/2_Ethiopia/1_Data_Clean/household_information_Ethiopia.csv")
